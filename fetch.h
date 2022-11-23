d8317a65f94107385b50cf71b92cca2f07b68443
Gabriel Kulp <kulpga@oregonstate.edu>
2022-11-23 14:29:33 -0800

    start fetch unit design

---
+++ fetch.h
@@ -0,0 +1,34 @@
+#include <systemc.h>
+#include "defines.h"
+#include "intersection.h"
+
+SC_MODULE(Fetch) {
+    sc_in<bool> rst;
+    sc_in<bool> clk;
+
+    // connection with control unit
+    sc_in<pointer_type> start_addr;
+    sc_in<pointer_type> end_addr;
+    
+    // connection with memory unit
+    //     request
+    sc_out<pointer_type> mem_req_address;
+    sc_in<bool> mem_req_ready;  //  mem is ready for next address
+    //     response
+    sc_in<tensor_element> mem_res_value;
+    sc_in<count_type> mem_res_index;
+    sc_out<bool> mem_res_ready;  // I'm ready for new outputs
+    sc_in<bool> mem_res_valid;  // mem's outputs are valid
+    
+    // connection with intersection (ixn) unit
+    Intersection * ixn;
+    sc_signal<bool> done;
+    
+    void fetch_main();
+    
+    SC_CTOR(Fetch) {
+        SC_THREAD(fetch_main);
+        reset_signal_is(rst, true);
+        sensitive << clk.pos();
+    }
+};
