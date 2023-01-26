16a9657adace6bb5a3da37fa8e396c6fc9b41df2
Gabriel Kulp <kulpga@oregonstate.edu>
2023-01-26 10:47:18 -0800

    finish first pass at control unit structs

--- hls/spec/types.cpp
+++ hls/spec/types.cpp
@@ -9,3 +9,21 @@ void sc_trace(sc_trace_file *& f, const fiber_entry & ent, std::string & name) {
     sc_trace(f, ent.index, name + ".idx");
     sc_trace(f, ent.value, name + ".val");
 }
+
+std::ostream & operator<< (std::ostream & os, const coord & c) {
+    os << "({" << c.order << "} ";
+    for (int i = 0; i < c.order-1; i++)
+        os << c[i] << ", ";
+    os << c[c.order-1] << ')';
+    return os;
+}
+
+void sc_trace(sc_trace_file *& f, const coord & c, std::string & name) {
+    sc_trace(f, c.order, name+".order");
+    std::stringstream _temp_ss;
+    for (int i = 0; i < c.order; i++) {
+        _temp_ss.clear();
+        _temp_ss << name << ".idx[" << c[i] << ']';
+        sc_trace(f, c.idx[i], _temp_ss.str().c_str());
+    }
+}
--- hls/spec/types.h
+++ hls/spec/types.h
@@ -45,9 +45,15 @@ struct coord {
     count_type idx[MAX_ORDER];
     const count_type order;
 
+    coord(count_type order) : order(order) {
+        if (order > MAX_ORDER)
+            throw new std::out_of_range("MAX_ORDER exceeded in coord init");
+    }
     coord(count_type * arr, count_type order) : order(order) {
         if (order > MAX_ORDER)
             throw new std::out_of_range("MAX_ORDER exceeded in coord init");
+        if (order < 1)
+            throw new std::out_of_range("coord can't havve order 0");
         for (int i = 0; i < order; i++)
             this->idx[i] = arr[i];
     }
@@ -60,7 +66,7 @@ struct coord {
         return *this;
     }
 
-    bool operator== (const coord &rhs) {
+    bool operator== (const coord &rhs) const {
         if (this->order != rhs.order)
             return false;
         for (int i = 0; i < rhs.order; i++)
@@ -69,32 +75,58 @@ struct coord {
         return true;
     }
     
-    bool zero() {
+    count_type operator[] (int i) { return this->idx[i]; }
+    const count_type operator[] (int i) const { return this->idx[i]; }
+    
+    bool zero() const {
         for (int i = 0; i < this->order; i++)
             if (this->idx[i])
                 return false;
         return true;
     }
     
-    //coord& operator++ ();
-    // doesn't work. Needs to know its maximum
+    coord concat(const coord &rhs) const {
+        coord new_coord = coord(this->order + rhs.order);
+        for (int i = 0; i < this->order; i++)
+            new_coord[i] = this->idx[i];
+        int j = 0;
+        for (int i = this->order; i < new_coord.order; i++)
+            new_coord[i] = rhs.idx[j++];
+        
+        return new_coord;
+    }
     
-    // other functions I might want:
-    //   concatenate coords
-    //   truncate
-    //   both of these together: make contraction destination index
-    
+    coord truncate(count_type drop_idx) const {
+        coord new_coord = coord(this->order-1);
+        int j = 0;
+        for (int i = 0; i < this->order; i++) {
+            if (i != drop_idx)
+                new_coord[j++] = this->idx[i];
+        }
+        return new_coord;
+    }
+
+    // make contraction destination index
+    coord contract(const coord &rhs, count_type lhs_contract, count_type rhs_contract) const {
+        return this->truncate(lhs_contract).concat(rhs.truncate(rhs_contract));
+    }
+
     friend std::ostream & operator<< (std::ostream & os, const coord & c);
 };
 
 struct tensor {
     coord shape;
     pointer_type fibers; // straightforward dense n-D array of fiber pointers
-    
+
+    tensor(coord _shape, pointer_type _fibers)
+        : shape(_shape)
+        , fibers(_fibers)
+    {}
+        
     bool increment(coord & c) {
-        if (this->shape.order == c.order) {
-            // increment through all indices
-            for (int i = 0; i < c.order; i++) {
+        if (this->shape.order == c.order || this->shape.order == c.order+1) {
+            // increment through all indices or all but last
+            for (int i = 0; i < this->shape.order; i++) {
                 if (this->shape.idx[i] < c.idx[i])
                     this->shape.idx[i]++;
                 if (this->shape.idx[i] < c.idx[i])
@@ -105,13 +137,7 @@ struct tensor {
                 }
             }
             return c.zero();
-        }
-        else if (this->shape.order == c.order + 1) {
-            // do not increment last index (fiber depth)
-            
-            return c.zero();
-        }
-        else {
+        } else {
             throw new std::out_of_range("cannot increment unrelated coord");
         }
     }
